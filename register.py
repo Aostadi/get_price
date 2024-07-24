f45f0186a74e2f071277a4ec287ad072085cffc4
abolfazl <proostadi@gmail.com>
2024-07-24 17:45:00 +0330

    add check_user_exit  function

--- register.py
+++ register.py
@@ -2,26 +2,34 @@ import mysql.connector
 import json
 
 
-def register(name, email):
-    try:
-        with open("info.json", "r") as js:
-            data = js.read()
-            data = json.loads(data)
+try:
+    with open("info.json", "r") as js:
+        data = js.read()
+        data = json.loads(data)
 
-        mydb = mysql.connector.connect(
-        host= data["server"]["host"],
-        user= data["server"]["user"],
-        password= data["server"]["password"],
-        database= data["server"]["database"]
-        )
-    except:
-        print("Can't connect to server")
+    mydb = mysql.connector.connect(
+    host= data["server"]["host"],
+    user= data["server"]["user"],
+    password= data["server"]["password"],
+    database= data["server"]["database"]
+    )
     cursor = mydb.cursor()
+except:
+    print("Can't connect to server")
+
+def register(name, email):
+    if check_user_exist():
+        print("you have registered before!")
+        exit()
+    else:
+        cursor.execute(f"insert into users (name, email) values ('{name}', '{email}')")
+
+def check_user_exist(email):
     cursor.execute(f"select name from users where email = '{email}'")
     result = cursor.fetchall()
     if result==list():
-        cursor.execute(f"insert into users (name, email) values ('{name}', '{email}')")
+        return True
     else:
-        print("you have registered before!")
-        exit()
+        return False
+    
 
